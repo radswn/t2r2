a1488d0d6c09e0573e4d718931ad9e3543ab981e
Radosław Winiecki <56360581+radswn@users.noreply.github.com>
2023-06-06 21:13:17 +0200

    Cleanup metrics - closes #7 (#22)
    * Optimize metrics configuration
    
    * Tiny name convention fixup

--- src/enginora/metrics/metrics.py
+++ src/enginora/metrics/metrics.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
+from typing import Dict, Any
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 
 def get_metric(name: str):
-    # TODO: add more metrics
     metrics = {
         'accuracy': accuracy_score,
+        'f1': f1_score
     }
 
     return metrics[name]
@@ -15,3 +16,8 @@ def get_metric(name: str):
 @dataclass
 class MetricsConfig:
     name: str
+    args: Dict[str, Any]
+
+    def __post_init__(self):
+        if self.args is None:
+            self.args = dict()
