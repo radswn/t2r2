f71b5e8bc77805aea9f910a400e41cc40026539f
ja-nina <zukowskanina1@gmail.com>
2023-12-12 23:34:29 +0100

    data config

---
+++ src/t2r2/config/common_params.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataConfig:
+    data_dir: str = "./data/"
+    output_dir: str = "./results/"
+    random_state: int = None
+    has_header: bool = False
+    text_column_id: 1
+    label_column_id: 2
