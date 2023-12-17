6700e6920eb46c6ab314545271993a342fb773ff
ja-nina <zukowskanina1@gmail.com>
2023-12-17 15:53:18 +0100

    nit comment removed

--- src/t2r2/selector/slicing/slicing.py
+++ src/t2r2/selector/slicing/slicing.py
@@ -19,7 +19,6 @@ class SlicingSelector(Selector):
         self.result_file = result_file
 
     def select(self, dataset: pd.DataFrame) -> pd.DataFrame:
-        # NIT: thinking about slicing in general: I highly doubt that is is truly a selector...
         check_if_directory_exists(self.result_file)
         if not len(self.sfs) == 0:
             res = self.create_slicing_functions(dataset)
