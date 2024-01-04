21faa6b789488ffc2da6a38cbc034e11ad216ab6
ja-nina <zukowskanina1@gmail.com>
2024-01-04 02:00:28 +0100

    fix lowering number of examples

--- src/t2r2/flow/flow.py
+++ src/t2r2/flow/flow.py
@@ -106,10 +106,10 @@ class T2R2:
     ) -> Dict[str, TextDataset]:
         training_dataset, validation_dataset = config.training.load_dataset()
         data = {
-            "train": training_dataset[:100],
-            "validation": validation_dataset[:20],
-            "test": config.testing.load_dataset()[:20],
-            "control": config.control.load_dataset()[:20],
+            "train": training_dataset,
+            "validation": validation_dataset,
+            "test": config.testing.load_dataset(),
+            "control": config.control.load_dataset(),
         }
 
         if mlflow_manager is not None:
